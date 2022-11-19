a6b656631fe4300f0d226265710ad607e96a06ba
Allan Li <allan0499@gmail.com>
2022-11-19 17:40:19 +0200

    Implement 1B

---
+++ section1/loading/script.js
@@ -0,0 +1,20 @@
+function changeSize() {
+  // Get DOM elements
+  const loading = document.getElementById('loading')
+  const loadingText = document.getElementById('loading-text')
+
+  // Use the toggle function to simplify class list checking
+  // Removes unnecessary if-else statements
+  loading.classList.toggle('loading-sm')
+  loadingText.classList.toggle('loading-text-none')
+}
+
+function changeTheme() {
+  const app = document.documentElement
+  app.classList.toggle('dark')
+}
+
+function changeSpeed() {
+  const loading = document.getElementById('loading')
+  loading.classList.toggle('loading-slow')
+}
