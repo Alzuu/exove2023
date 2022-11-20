6c937c14e75a46427b5f7234c72ee63229348e67
Allan Li <allan0499@gmail.com>
2022-11-20 13:55:21 +0200

    Implement theme toggle (light and dark)

--- section3/react-carousel/src/App.tsx
+++ section3/react-carousel/src/App.tsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
+import { FiSun, FiMoon } from 'react-icons/fi'
 import ImageHandler from './components/ImageHandler'
 import ImageCarousel from './components/ImageCarousel'
 import './App.css'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  const root = document.documentElement
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode)
+    root.classList.toggle('dark-theme')
+  }
+
   return (
     <div className='App'>
-      <h1>Helvetinjärvi 2022</h1>
+      <section className='header'>
+        <h1>Helvetinjärvi 2022</h1>
+        {darkMode ? (
+          <FiSun className='theme-toggle' onClick={toggleTheme}></FiSun>
+        ) : (
+          <FiMoon className='theme-toggle' onClick={toggleTheme}></FiMoon>
+        )}
+      </section>
       <ImageCarousel images={ImageHandler}></ImageCarousel>
     </div>
   )
